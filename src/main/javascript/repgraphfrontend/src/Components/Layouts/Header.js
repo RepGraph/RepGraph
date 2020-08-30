ee3ee291fed868f56954a20c29c38752169b7b54
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-08-30 15:54:28 +0200

    Create Header.js file in Components/Layouts with simple Material UI AppBar.

---
+++ src/main/javascript/repgraphfrontend/src/Components/Layouts/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginBottom:10,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    }
+}));
+
+function Header(){
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" >
+                <Toolbar >
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        RepGraph
+                    </Typography>
+
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+
+}
+export default Header;
