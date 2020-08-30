50f40be385d87b4ef0465b68707bc110a428e3a1
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-08-30 17:49:28 +0200

    Create SearchSubgraphPatternTool component to handle selection of node labels to search against the graphs in the dataset.

---
+++ src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/SearchSubgraphPatternTool.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Chip from '@material-ui/core/Chip';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "80%",
+        '& > * + *': {
+            marginTop: theme.spacing(3),
+        },
+    },
+    autoComplete :{
+        marginBottom: 10
+    }
+}));
+
+function SearchSubgraphPatternTool(props) {
+    const classes = useStyles();
+
+    const nodeLabels = props.sentence.nodes.map(node => node.label);
+
+    return (
+        <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+        >
+        <div className={classes.root}>
+            <Autocomplete className={classes.autoComplete}
+                          multiple
+                          id="tags-outlined"
+                          options={nodeLabels}
+                          getOptionLabel={(option) => option}
+                          defaultValue={[nodeLabels[0]]}
+                          filterSelectedOptions
+                          renderInput={(params) => (
+                              <TextField
+                                  {...params}
+                                  variant="outlined"
+                                  label="Node Labels:"
+                                  placeholder="Selected Node Labels"
+                              />
+                          )}
+            />
+        </div>
+        </Grid>
+    );
+}
+export default SearchSubgraphPatternTool;
