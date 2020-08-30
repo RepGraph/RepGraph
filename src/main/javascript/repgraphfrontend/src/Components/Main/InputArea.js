e9ea75ef869210ae17493df0629b8869be619d30
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-08-30 16:44:58 +0200

    Create AnalysisAccordion component within InputArea.js to encapsulate the UI components that will handle the various analysis tool functionality.

--- src/main/javascript/repgraphfrontend/src/Components/Main/InputArea.js
+++ src/main/javascript/repgraphfrontend/src/Components/Main/InputArea.js
@@ -7,6 +7,12 @@ import Tooltip from "@material-ui/core/Tooltip";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Zoom from "@material-ui/core/Zoom";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const styles = {
     Paper: {
@@ -58,10 +64,83 @@ class InputArea extends React.Component
                 <Divider></Divider>
                 <Typography variant="h6" align="center">Analysis Tools:</Typography>
                 <Divider></Divider>
-                Analysis Tools here
+                <AnalysisAccordion/>
             </Paper>
         );
     }
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%'
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
+
+function AnalysisAccordion(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="display-subset-header"
+                >
+                    <Typography className={classes.heading}>Display a subset of a graph</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Select a node on the graph displayed in the visualization area to see the corresponding subset of the graph:</Typography>
+                </AccordionDetails>
+                Display subset tool to go here.
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="search-sub-graph-header"
+                >
+                    <Typography className={classes.heading}>Search for a sub-graph pattern</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Manually select a number of node labels to search against the uploaded data-set or visually select a subgraph pattern of a graph displayed in the Visualization Area on the right.</Typography>
+                </AccordionDetails>
+                Subgraph pattern tool to go here.
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="compare-two-header"
+                >
+                    <Typography className={classes.heading}>Compare Two Graphs</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Select a two graphs below:</Typography>
+                </AccordionDetails>
+                Compare graphs tool to go here.
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="run-formal-header"
+                >
+                    <Typography className={classes.heading}>Run Formal Tests</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>Select a number of graph properties with which to test the currently displayed graph:</Typography>
+                </AccordionDetails>
+                Formal tests tool to go here.
+
+            </Accordion>
+
+        </div>
+    );
 }
 
 export default InputArea;
