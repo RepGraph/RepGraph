09c538c32f517c04df7218f1313ac87277f0d997
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-09-08 18:47:21 +0200

    Implement algorithm to rank the nodes in the visualization by their spans in VisualizerArea.js

--- src/main/javascript/repgraphfrontend/src/Components/Main/VisualizerArea.js
+++ src/main/javascript/repgraphfrontend/src/Components/Main/VisualizerArea.js
@@ -39,10 +39,31 @@ const testGraph = JSON.parse(
   '{"id":"20001001","source":"wsj00a","input":"Pierre Vinken, 61 years old, will join the board as a nonexecutive director Nov. 29.","tokens":[{"index":0,"form":"pierre","lemma":"Pierre","carg":"Pierre"},{"index":1,"form":"Vinken,","lemma":"Vinken","carg":"Vinken"},{"index":2,"form":"61","lemma":"61","carg":"61"},{"index":3,"form":"years","lemma":"year"},{"index":4,"form":"old,","lemma":"old"},{"index":5,"form":"will","lemma":"will"},{"index":6,"form":"join","lemma":"join"},{"index":7,"form":"the","lemma":"the"},{"index":8,"form":"board","lemma":"board"},{"index":9,"form":"as","lemma":"as"},{"index":10,"form":"a","lemma":"a"},{"index":11,"form":"nonexecutive","lemma":"nonexecutive"},{"index":12,"form":"director","lemma":"director"},{"index":13,"form":"nov.","lemma":"Nov","carg":"Nov"},{"index":14,"form":"29.","lemma":"29","carg":"29"}],"nodes":[{"id":0,"label":"proper_q","anchors":[{"from":0,"end":0}]},{"id":1,"label":"named","anchors":[{"from":0,"end":0}]},{"id":2,"label":"named","anchors":[{"from":1,"end":1}]},{"id":3,"label":"compound","anchors":[{"from":0,"end":1}]},{"id":4,"label":"card","anchors":[{"from":2,"end":2}]},{"id":5,"label":"_year_n_1","anchors":[{"from":3,"end":3}]},{"id":6,"label":"measure","anchors":[{"from":2,"end":3}]},{"id":7,"label":"udef_q","anchors":[{"from":2,"end":3}]},{"id":8,"label":"_old_a_1","anchors":[{"from":4,"end":4}]},{"id":9,"label":"proper_q","anchors":[{"from":0,"end":4}]},{"id":10,"label":"_join_v_1","anchors":[{"from":6,"end":6}]},{"id":11,"label":"_the_q","anchors":[{"from":7,"end":7}]},{"id":12,"label":"_board_n_of","anchors":[{"from":8,"end":8}]},{"id":13,"label":"_as_p","anchors":[{"from":9,"end":9}]},{"id":14,"label":"_a_q","anchors":[{"from":10,"end":10}]},{"id":15,"label":"_nonexecutive_u_unknown","anchors":[{"from":11,"end":11}]},{"id":16,"label":"_director_n_of","anchors":[{"from":12,"end":12}]},{"id":17,"label":"mofy","anchors":[{"from":13,"end":13}]},{"id":18,"label":"def_explicit_q","anchors":[{"from":13,"end":13}]},{"id":19,"label":"of_p","anchors":[{"from":13,"end":13}]},{"id":20,"label":"def_implicit_q","anchors":[{"from":13,"end":13}]},{"id":21,"label":"dofm","anchors":[{"from":14,"end":14}]},{"id":22,"label":"loc_nonsp","anchors":[{"from":13,"end":14}]}],"edges":[{"source":9,"target":2,"label":"RSTR","post-label":"H"},{"source":3,"target":2,"label":"ARG1","post-label":"EQ"},{"source":3,"target":1,"label":"ARG2","post-label":"NEQ"},{"source":0,"target":1,"label":"RSTR","post-label":"H"},{"source":6,"target":8,"label":"ARG1","post-label":"EQ"},{"source":6,"target":5,"label":"ARG2","post-label":"NEQ"},{"source":7,"target":5,"label":"RSTR","post-label":"H"},{"source":4,"target":5,"label":"ARG1","post-label":"EQ"},{"source":8,"target":2,"label":"ARG1","post-label":"EQ"},{"source":10,"target":2,"label":"ARG1","post-label":"NEQ"},{"source":10,"target":12,"label":"ARG2","post-label":"NEQ"},{"source":11,"target":12,"label":"RSTR","post-label":"H"},{"source":13,"target":10,"label":"ARG1","post-label":"EQ"},{"source":13,"target":16,"label":"ARG2","post-label":"NEQ"},{"source":14,"target":16,"label":"RSTR","post-label":"H"},{"source":15,"target":16,"label":"ARG1","post-label":"EQ"},{"source":22,"target":10,"label":"ARG1","post-label":"EQ"},{"source":22,"target":21,"label":"ARG2","post-label":"NEQ"},{"source":18,"target":21,"label":"RSTR","post-label":"H"},{"source":19,"target":21,"label":"ARG1","post-label":"EQ"},{"source":19,"target":17,"label":"ARG2","post-label":"NEQ"},{"source":20,"target":17,"label":"RSTR","post-label":"H"}],"tops":[10]}'
 );
 
+const testGraphNodeSpanLengths = testGraph.nodes
+  .map((node) => node.anchors[0])
+  .map((span) => span.end - span.from);
+
+let uniqueSpans = [];
+const map = new Map();
+for (const item of testGraphNodeSpanLengths) {
+  if (!map.has(item)) {
+    map.set(item, true); // set any value to Map
+    uniqueSpans.push(item);
+  }
+}
+uniqueSpans.sort((a, b) => a - b); //sort unique spans ascending
+
+function getNodeLevel(id) {
+  const nodeSpan = testGraphNodeSpanLengths[id];
+  return uniqueSpans.findIndex((element) => element === nodeSpan);
+}
+
 const testGraphNodes = testGraph.nodes.map((node) => ({
-  x: Math.random() * 780,
-  y: Math.random() * 500,
+  id: node.id,
+  x: node.id * 90,
+  y: 400 - getNodeLevel(node.id) * (400 / (uniqueSpans.length - 1)),
   label: node.label,
+  type: "node",
 }));
 
 const testGraphEdges = testGraph.edges.map((edge) => ({
@@ -50,14 +71,16 @@ const testGraphEdges = testGraph.edges.map((edge) => ({
   target: testGraphNodes[edge.target],
 }));
 
-const numTokens = testGraph.tokens.length;
-
-const testGraphTokens = testGraph.tokens;
-
-//.map((token) => ({index: token.index, form: token.form}));
+const testGraphTokens = testGraph.tokens.map((token) => ({
+  index: token.index,
+  x: token.index * 90,
+  y: 550,
+  label: token.form,
+  type: "token",
+}));
 
 const testGraphData = {
-  nodes: testGraphNodes,
+  nodes: testGraphNodes.concat(testGraphTokens),
   links: testGraphEdges,
 };
 
@@ -85,7 +108,7 @@ const Node = ({ node }) => {
         ry="10"
         width="80"
         height="30"
-        fill="#FFE521"
+        fill={node.type === "node" ? "#ffea00" : "#00e676"}
         stroke-width="0"
         stroke="#000"
       />
@@ -155,7 +178,7 @@ function Visualization(props) {
                 width={props.width}
                 height={props.height}
                 rx={14}
-                fill="#272b4d"
+                fill="#3f51b5"
               />
               <g transform={zoom.toString()}>
                 <Graph
@@ -405,7 +428,9 @@ class VisualizerArea extends React.Component {
           value={
             JSON.stringify(testGraphNodes) +
             "\n" +
-            JSON.stringify(testGraphEdges)
+            JSON.stringify(testGraphEdges) +
+            "\n" +
+            JSON.stringify(testGraphNodeSpanLengths)
           }
         />
 
