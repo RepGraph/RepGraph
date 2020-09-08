c4197bddd6200ac7e76e354f5a8835ba216b263d
Edan Toledo <TLDEDA001@myuct.ac.za>
2020-09-08 23:46:23 +0200

    Add Anchors class. This class is apart of the node class and is used for node data. The reason for this class is so there can be automatic serialisation and deserialisation of graph objects from the front-end to back-end.
    Signed-off-by: Edan Toledo <TLDEDA001@myuct.ac.za>

---
+++ src/main/java/com/RepGraph/anchors.java
@@ -0,0 +1,29 @@
+package com.RepGraph;
+
+public class anchors {
+    private String from;
+    private String end;
+
+    public anchors(String from, String end) {
+        this.from = from;
+        this.end = end;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+
+}
