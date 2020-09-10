b8e64b624f2cc090b0410d93756ab033b9b0041f
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-09-10 17:51:25 +0200

    Add serviceWorker.js back to project

--- src/main/javascript/repgraphfrontend/src/index.js
+++ src/main/javascript/repgraphfrontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import * as serviceWorker from './oldComponents/serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
