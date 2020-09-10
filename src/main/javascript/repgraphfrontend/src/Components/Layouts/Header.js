c233db5b3d1f6bf3e6f35b32e4c6dd867d4c152d
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-09-10 17:43:47 +0200

    Add upload a data-set button  to the  app bar
    - Creates a button in the app bar that will open an upload file dialog when clicked by the user

--- src/main/javascript/repgraphfrontend/src/Components/Layouts/Header.js
+++ src/main/javascript/repgraphfrontend/src/Components/Layouts/Header.js
@@ -5,6 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import DropZoneUploadDataset from "../UploadDatasetDialogue";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -30,7 +31,7 @@ function Header(){
                     <Typography variant="h6" className={classes.title}>
                         RepGraph
                     </Typography>
-
+                    <DropZoneUploadDataset></DropZoneUploadDataset>
                 </Toolbar>
             </AppBar>
         </div>
