508b4ad13dc42d1c8924067f143f1c22b455c703
Edan Toledo <TLDEDA001@myuct.ac.za>
2020-09-13 15:36:14 +0200

    Add Unit Test for parameter constructor.
    -Test case checks that the parameter constructor correctly assigns the values specified into the correct member variables.
    
    Signed-off-by: Edan Toledo <TLDEDA001@myuct.ac.za>

--- src/test/java/com/RepGraph/EdgeTest.java
+++ src/test/java/com/RepGraph/EdgeTest.java
@@ -8,6 +8,29 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 public class EdgeTest {
 
+    @Test
+    public void test_constructor_CreatesAndAssignsMemberVariables() throws NoSuchFieldException, IllegalAccessException {
+
+        final edge e = new edge(3, 5, "test", "postTest");
+
+        final Field source = e.getClass().getDeclaredField("source");
+        source.setAccessible(true);
+
+        final Field target = e.getClass().getDeclaredField("target");
+        target.setAccessible(true);
+
+        final Field label = e.getClass().getDeclaredField("label");
+        label.setAccessible(true);
+
+        final Field postlabel = e.getClass().getDeclaredField("postLabel");
+        postlabel.setAccessible(true);
+
+        assertEquals("source value was not assigned properly in constructor", source.get(e), 3);
+        assertEquals("target value was not assigned properly in constructor", target.get(e), 5);
+        assertEquals("label value was not assigned properly in constructor", label.get(e), "test");
+        assertEquals("postlabel value was not assigned properly in constructor", postlabel.get(e), "postTest");
+
+    }
 
     @Test
     public void test_getSource_GetsValueCorrectly() throws NoSuchFieldException, IllegalAccessException {
