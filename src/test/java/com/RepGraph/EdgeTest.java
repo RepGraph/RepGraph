2c7eedf5ebd3413ddc428d7337c53609c25b8608
Edan Toledo <TLDEDA001@myuct.ac.za>
2020-09-13 14:55:07 +0200

    Add Unit test for "getSource" getter method in edge class.
    - Test case to see if the getter method correctly returns the member variable value.
    
    Signed-off-by: Edan Toledo <TLDEDA001@myuct.ac.za>

--- src/test/java/com/RepGraph/EdgeTest.java
+++ src/test/java/com/RepGraph/EdgeTest.java
@@ -9,4 +9,23 @@ import static org.springframework.test.util.AssertionErrors.assertEquals;
 public class EdgeTest {
 
 
+    @Test
+    public void tests_getSource_GetsValueCorrectly() throws NoSuchFieldException, IllegalAccessException {
+
+        final edge e = new edge();
+
+        //Set Field not using setter
+        final Field field = e.getClass().getDeclaredField("source");
+        field.setAccessible(true);
+        field.set(e, 3);
+
+
+        final int result = e.getSource();
+
+        //then
+        assertEquals("field wasn't retrieved properly", result, 3);
+
+    }
+
+
 }
