462d98ee479fca112136dda04412cea5f432d84d
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-09-29 12:18:00 +0200

    Add dialog to select node from graph in DisplaySubsetTool

--- src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/DisplaySubsetTool.js
+++ src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/DisplaySubsetTool.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Grid from "@material-ui/core/Grid";
 import {makeStyles} from "@material-ui/core/styles";
 import FormLabel from "@material-ui/core/FormLabel";
@@ -9,6 +9,15 @@ import Radio from "@material-ui/core/Radio";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import Button from "@material-ui/core/Button";
 import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+
+import { AppContext } from "../../Store/AppContextProvider.js";
+import {useHistory} from "react-router-dom";
+import GraphVisualisation from "../Main/GraphVisualisation";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,6 +29,18 @@ const useStyles = makeStyles((theme) => ({
 
 
 function DisplaySubsetTool(props){
+    const { state, dispatch } = useContext(AppContext);
+    const history = useHistory();
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const [value, setValue] = React.useState('adjacent');
 
@@ -28,6 +49,10 @@ function DisplaySubsetTool(props){
     };
 
     const classes = useStyles();
+
+    function handleDisplaySubset(){
+        console.log();
+    }
 
     return(
         <Grid
@@ -44,13 +69,34 @@ function DisplaySubsetTool(props){
                     <FormControlLabel value="descendent" control={<Radio color="primary"/>} label="Display Descendent Nodes" />
                 </RadioGroup>
                 <Button
-                    variant="contained"
-                    color="primary"
+                    variant="outlined" color="primary" onClick={handleClickOpen}
                     endIcon={<LocationSearchingIcon/>}
-                    onClick={props.onClick}
                 >
                     Select Subset
                 </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Select a node on the graph:"}</DialogTitle>
+                    <DialogContent>
+                        {state.selectedSentence === null ? (
+                            <div>Select a sentence first.</div>
+                        ) : (
+                            <GraphVisualisation />
+                        )}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Disagree
+                        </Button>
+                        <Button onClick={handleClose} color="primary" autoFocus>
+                            Agree
+                        </Button>
+                    </DialogActions>
+                </Dialog>
             </FormControl>
         </Grid>
     );
