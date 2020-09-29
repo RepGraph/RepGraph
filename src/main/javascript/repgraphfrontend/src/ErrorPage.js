a110d459e1a628e2af7b69e323bd4a75dbefeed9
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-09-29 13:41:32 +0200

    Improve styling for Error Page in ErrorPage.js

--- src/main/javascript/repgraphfrontend/src/ErrorPage.js
+++ src/main/javascript/repgraphfrontend/src/ErrorPage.js
@@ -1,6 +1,26 @@
-import { Button } from "@material-ui/core";
+import {Button, Grid, Typography} from "@material-ui/core";
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import {DropzoneArea} from "material-ui-dropzone";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 export default function ErrorPage() {
-  return <Button>Hello</Button>;
+  const history = useHistory();
+  return (
+      <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          style={{ minHeight: "100vh", minWidth: "100vw" }}
+          spacing={2}
+      >
+        <Grid item>
+          <Typography variant="h2">Error 404</Typography>
+        </Grid>
+        <Grid item>
+          <Button onClick={() => {history.push("/")}}>Take Me Home</Button>
+        </Grid>
+      </Grid>
+      );
 }
