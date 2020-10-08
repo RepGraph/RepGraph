8c23d6891b3818fab643108589daef135e6fbe32
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-10-08 23:15:54 +0200

    Modify DisplaySubsetTool to display requested subset result in separate dialog and not modify global state. Create Modified GraphVisualisation component called SubsetVisualisation to display subset result in dialog.

--- src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/DisplaySubsetTool.js
+++ src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/DisplaySubsetTool.js
@@ -21,6 +21,7 @@ import GraphVisualisation from "../Main/GraphVisualisation";
 import Chip from "@material-ui/core/Chip";
 
 import {layoutGraph} from "../App";
+import SubsetVisualisation from "../Main/SubsetVisualisation";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -33,9 +34,12 @@ const useStyles = makeStyles((theme) => ({
 
 function DisplaySubsetTool(props){
     const { state, dispatch } = useContext(AppContext);
+    const [subsetResponse, setSubsetResponse] = React.useState(null);
+
     const history = useHistory();
 
     const [open, setOpen] = React.useState(false);
+    const [resultOpen, setResultOpen] = React.useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -43,6 +47,9 @@ function DisplaySubsetTool(props){
 
     const handleClose = () => {
         setOpen(false);
+    };
+    const handleResultClose = () => {
+        setResultOpen(false);
     };
 
     const [value, setValue] = React.useState('adjacent');
@@ -61,17 +68,24 @@ function DisplaySubsetTool(props){
             redirect: 'follow'
         };
 
-        console.log(state.selectedSentenceID, state.selectedNodeAndEdges.nodes[0]);
+        console.log(state.selectedSentenceID, state.selectedNodeAndEdges.nodes[0], value);
         dispatch({ type: "SET_LOADING", payload: { isLoading: true } });
 
-        fetch(state.APIendpoint+"/DisplaySubset?graphID="+state.selectedSentenceID+"&NodeID="+state.selectedNodeAndEdges.nodes[0], requestOptions)
+        fetch(state.APIendpoint+"/DisplaySubset?graphID="+state.selectedSentenceID+"&NodeID="+state.selectedNodeAndEdges.nodes[0]+"&SubsetType="+value, requestOptions)
             .then((response) => response.text())
             .then((result) => {
                 console.log(result);//debugging
+                console.log(JSON.parse(result));//debugging
                 const jsonResult = JSON.parse(result);
-                const formattedGraph = layoutGraph(jsonResult);
-                dispatch({ type: "SET_SENTENCE", payload: { selectedSentence: formattedGraph } });
+
+                const formattedGraph = layoutGraph(jsonResult); //will remove this step before final
+
+                //dispatch({type: "SET_SENTENCE_VISUALISATION", payload: {selectedSentenceVisualisation: formattedGraph}});
+                //dispatch({type: "SET_SELECTED_SENTENCE_ID", payload: {selectedSentenceID: jsonResult.id}});
+                setSubsetResponse(formattedGraph);
+                setResultOpen(true);
                 dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
+
             })
             .catch((error) => {
                 dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
@@ -97,18 +111,21 @@ function DisplaySubsetTool(props){
                 <Button
                     variant="contained" color="primary" onClick={handleClickOpen}
                     endIcon={<LocationSearchingIcon/>}
+                    disabled={state.selectedSentenceID === null}
                 >
                     Select Subset
                 </Button>
                 <Dialog
                     open={open}
+                    fullWidth={true}
+                    maxWidth="xl"
                     onClose={handleClose}
                     aria-labelledby="alert-dialog-title"
                     aria-describedby="alert-dialog-description"
                 >
                     <DialogTitle id="alert-dialog-title">{"Select a node on the graph:"}</DialogTitle>
-                    <DialogContent>
-                        {state.selectedSentence === null ? (
+                    <DialogContent style={{height:"80vh"}}>
+                        {state.selectedSentenceID === null ? (
                             <div>Select a sentence first.</div>
                         ) : (
                             <GraphVisualisation />
@@ -116,8 +133,30 @@ function DisplaySubsetTool(props){
                     </DialogContent>
                     <DialogActions>
                         <Chip label= {`Selected Node: ${JSON.stringify(state.selectedNodeAndEdges)}`} />
-                        <Button onClick={handleDisplaySubset} color="primary" autoFocus>
+                        <Button onClick={handleDisplaySubset} color="primary" autoFocus >
                             Display
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+                <Dialog
+                    open={resultOpen}
+                    fullWidth={true}
+                    maxWidth="xl"
+                    onClose={handleResultClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Subset result based upon node selection:"}</DialogTitle>
+                    <DialogContent style={{height:"80vh"}}>
+                        {subsetResponse === null ? (
+                            <div>No result to display</div>
+                        ) : (
+                            <SubsetVisualisation subsetGraphData={subsetResponse}/>
+                        )}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleResultClose} color="primary" autoFocus >
+                            close
                         </Button>
                     </DialogActions>
                 </Dialog>
