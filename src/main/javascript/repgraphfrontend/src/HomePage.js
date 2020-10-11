fc7c40f10e52cc692240af4f1e68d3d73b5cdfbe
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-10-12 01:44:50 +0200

    Add alert to HomePage.js to inform user when selecting a file to upload

--- src/main/javascript/repgraphfrontend/src/HomePage.js
+++ src/main/javascript/repgraphfrontend/src/HomePage.js
@@ -5,6 +5,8 @@ import { Link, useHistory } from "react-router-dom";
 import { DropzoneArea } from "material-ui-dropzone";
 
 import { AppContext } from "./Store/AppContextProvider.js";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import { dmrsData } from "./store.js";
 
@@ -13,45 +15,63 @@ const testingEndpoint = "http://localhost:8080";
 export default function HomePage(props) {
   const [isUploading, setIsUploading] = useState(false);
   const [fileObjects, setFileObjects] = useState([]);
+  const [open, setOpen] = React.useState(false);
+
   const history = useHistory();
 
   const { dispatch } = useContext(AppContext);
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   function handleChange(files) {
-    setFileObjects({ ...fileObjects, ...files });
+    setFileObjects([ ...fileObjects, ...files ]);
   }
 
   function handleUpload() {
-    let formData = new FormData();
-    formData.append("data", fileObjects[0]);
+    console.log(fileObjects);
+    if(fileObjects.length > 0){
+      let formData = new FormData();
+      formData.append("data", fileObjects[0]);
 
-    let requestOptions = {
-      method: "POST",
-      body: formData,
-      redirect: "follow"
-    };
+      let requestOptions = {
+        method: "POST",
+        body: formData,
+        redirect: "follow"
+      };
 
-    dispatch({ type: "SET_LOADING", payload: { isLoading: true } });
+      dispatch({ type: "SET_LOADING", payload: { isLoading: true } });
 
-    fetch(
-      testingEndpoint + "/UploadData?FileName=SupremeLeaderJanBuys",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => {
-        const jsonResult = JSON.parse(result);
-        console.log(jsonResult);
-        console.log(jsonResult.response);
-        dispatch({ type: "SET_DATASET", payload: { dataSet: jsonResult.data } });
-        dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-        //this.setState({sentences: jsonResult.data, sentence: jsonResult.data[0]});
-        history.push("/main");
-      })
-      .catch((error) => {
-        console.log("error", error);
-        dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-        history.push("/404"); //for debugging
-      });
+      fetch(
+          testingEndpoint + "/UploadData?FileName=SupremeLeaderJanBuys",
+          requestOptions
+      )
+          .then((response) => response.text())
+          .then((result) => {
+            const jsonResult = JSON.parse(result);
+            console.log(jsonResult);
+            console.log(jsonResult.response);
+            dispatch({ type: "SET_DATASET", payload: { dataSet: jsonResult.data } });
+            dispatch({ type: "SET_DATASET_FILENAME", payload: { dataSetFileName: fileObjects[0].name } });
+            dispatch({ type: "SET_DATASET_RESPONSE", payload: { dataSetResponse: jsonResult.response } });
+            dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
+
+            //this.setState({sentences: jsonResult.data, sentence: jsonResult.data[0]});
+            history.push("/main");
+          })
+          .catch((error) => {
+            console.log("error", error);
+            dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
+            history.push("/404"); //for debugging
+          });
+    }else {
+    setOpen(true);
+    }
   }
 
   return (
@@ -85,6 +105,11 @@ export default function HomePage(props) {
           <Typography variant="h6">Upload</Typography>
         </Button>
       </Grid>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <MuiAlert elevation={6} variant="filled" onClose={handleClose} severity="warning">
+          Please select a data-set first.
+        </MuiAlert>
+      </Snackbar>
     </Grid>
   );
 }
