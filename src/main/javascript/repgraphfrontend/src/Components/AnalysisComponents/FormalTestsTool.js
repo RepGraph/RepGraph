8a6216e54b1d5e0d51f1f1ed1e59dbf3919fc2b0
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-10-12 19:35:50 +0200

    Add comments to FormalTestsTool.js

--- src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/FormalTestsTool.js
+++ src/main/javascript/repgraphfrontend/src/Components/AnalysisComponents/FormalTestsTool.js
@@ -11,13 +11,6 @@ import Grid from "@material-ui/core/Grid";
 import {makeStyles} from "@material-ui/core/styles";
 import {AppContext} from "../../Store/AppContextProvider";
 import {useHistory} from "react-router-dom";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import GraphVisualisation from "../Main/GraphVisualisation";
-import DialogActions from "@material-ui/core/DialogActions";
-import Chip from "@material-ui/core/Chip";
-import Dialog from "@material-ui/core/Dialog";
-import FormalTestResultsDisplay from "./FormalTestResultsDisplay";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,14 +20,12 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-function FormalTestsTool(props){
-    const { state, dispatch } = useContext(AppContext);
-    const history = useHistory();
+function FormalTestsTool(props) {
+    const {state, dispatch} = useContext(AppContext); //Provide access to global state
+    const history = useHistory(); //Provide access to router history
 
-    const classes = useStyles();
-
-    const [testResults, setTestResults ] = React.useState(null);
-
+    const classes = useStyles(); //Use styles created above
+    //Local state for checkboxes of which tests to perform
     const [tests, setTests] = React.useState({
         planar: false,
         longestPathDirected: false,
@@ -42,41 +33,38 @@ function FormalTestsTool(props){
         connected: false
     });
 
-
+    //Handle change to checkboxes for formal tests
     const handleChange = (event) => {
-        setTests({ ...tests, [event.target.name]: event.target.checked });
+        setTests({...tests, [event.target.name]: event.target.checked});
     };
 
-
-    function handleFormalTests(){
+    //Handle request to perform formal tests
+    function handleFormalTests() {
 
         let requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
 
-        dispatch({ type: "SET_LOADING", payload: { isLoading: true } });
+        dispatch({type: "SET_LOADING", payload: {isLoading: true}}); //Show loading animation
 
-        fetch(state.APIendpoint+"/TestGraph?graphID="+state.selectedSentenceID+"&planar="+tests.planar+"&longestPathDirected="+tests.longestPathDirected+"&longestPathUndirected="+tests.longestPathUndirected+"&connected="+tests.connected, requestOptions)
+        //Fetch formal test results from backend
+        fetch(state.APIendpoint + "/TestGraph?graphID=" + state.selectedSentenceID + "&planar=" + tests.planar + "&longestPathDirected=" + tests.longestPathDirected + "&longestPathUndirected=" + tests.longestPathUndirected + "&connected=" + tests.connected, requestOptions)
             .then((response) => response.text())
             .then((result) => {
-
                 const jsonResult = JSON.parse(result);
-                console.log(jsonResult);
-                dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-                dispatch({ type: "SET_TEST_RESULTS", payload: { testResults: jsonResult } });
-                setTestResults(jsonResult);
+                console.log(jsonResult); //Debugging
+                dispatch({type: "SET_LOADING", payload: {isLoading: false}}); //Stop the loading animation
+                dispatch({type: "SET_TEST_RESULTS", payload: {testResults: jsonResult}}); //Store the test results in global state
             })
             .catch((error) => {
-                dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-                console.log("error", error);
-                history.push("/404"); //for debugging
+                dispatch({type: "SET_LOADING", payload: {isLoading: false}}); //Stop the loading animation
+                console.log("error", error); //Log the error to the console
+                history.push("/404"); //Take the user to the error page
             });
     }
 
-    const { planar, longestPathDirected, longestPathUndirected, connected } = state;
-
-    return(
+    return (
         <Grid
             container
             direction="column"
@@ -88,19 +76,23 @@ function FormalTestsTool(props){
                 <FormLabel color="primary" component="legend">Graph Property Tests</FormLabel>
                 <FormGroup>
                     <FormControlLabel
-                        control={<Checkbox checked={planar} onChange={handleChange} color="secondary"  name="planar" />}
+                        control={<Checkbox checked={tests.planar} onChange={handleChange} color="secondary"
+                                           name="planar"/>}
                         label="Graph Planar?"
                     />
                     <FormControlLabel
-                        control={<Checkbox  checked={longestPathDirected} onChange={handleChange} color="secondary" name="longestPathDirected" />}
+                        control={<Checkbox checked={tests.longestPathDirected} onChange={handleChange} color="secondary"
+                                           name="longestPathDirected"/>}
                         label="Find Longest Directed Path"
                     />
                     <FormControlLabel
-                        control={<Checkbox  checked={longestPathUndirected} onChange={handleChange} color="secondary" name="longestPathUndirected" />}
+                        control={<Checkbox checked={tests.longestPathUndirected} onChange={handleChange}
+                                           color="secondary" name="longestPathUndirected"/>}
                         label="Find Longest Undirected Path"
                     />
                     <FormControlLabel
-                        control={<Checkbox  checked={connected} onChange={handleChange} color="secondary" name="connected" />}
+                        control={<Checkbox checked={tests.connected} onChange={handleChange} color="secondary"
+                                           name="connected"/>}
                         label="Graph Connected?"
                     />
                     <Button
@@ -108,8 +100,10 @@ function FormalTestsTool(props){
                         variant="contained"
                         color="primary"
                         endIcon={<ArrowForwardIcon/>}
-                        onClick={() => {console.log(tests);
-                        handleFormalTests();}}
+                        onClick={() => {
+                            console.log(tests); //Debugging
+                            handleFormalTests();
+                        }}
                     >
                         Run Tests
                     </Button>
