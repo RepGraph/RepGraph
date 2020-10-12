71d217708db98276ad3d542069a96fba1679c11b
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-10-12 16:36:57 +0200

    Fix APIEndpoint in HomePage.js to utilise global state endpoint. Add comments to HomePage.js

--- src/main/javascript/repgraphfrontend/src/HomePage.js
+++ src/main/javascript/repgraphfrontend/src/HomePage.js
@@ -3,24 +3,17 @@ import { Card, Typography, Button, Grid } from "@material-ui/core";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import { Link, useHistory } from "react-router-dom";
 import { DropzoneArea } from "material-ui-dropzone";
-
 import { AppContext } from "./Store/AppContextProvider.js";
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
 
-import { dmrsData } from "./store.js";
-
-const testingEndpoint = "http://localhost:8080";
-
 export default function HomePage(props) {
-  const [isUploading, setIsUploading] = useState(false);
   const [fileObjects, setFileObjects] = useState([]);
   const [open, setOpen] = React.useState(false);
-
   const history = useHistory();
+  const { state, dispatch } = useContext(AppContext);
 
-  const { dispatch } = useContext(AppContext);
-
+  //Handle close for the alert shown to user
   const handleClose = (event, reason) => {
     if (reason === 'clickaway') {
       return;
@@ -29,12 +22,13 @@ export default function HomePage(props) {
     setOpen(false);
   };
 
+  //Handle change when user selects a file
   function handleChange(files) {
     setFileObjects([ ...fileObjects, ...files ]);
   }
 
   function handleUpload() {
-    console.log(fileObjects);
+    console.log(fileObjects); //Debugging
     if(fileObjects.length > 0){
       let formData = new FormData();
       formData.append("data", fileObjects[0]);
@@ -45,29 +39,28 @@ export default function HomePage(props) {
         redirect: "follow"
       };
 
-      dispatch({ type: "SET_LOADING", payload: { isLoading: true } });
+      dispatch({ type: "SET_LOADING", payload: { isLoading: true } }); //Show loading animation while awaiting response
 
+      //Upload the file chosen by the user
       fetch(
-          testingEndpoint + "/UploadData?FileName=SupremeLeaderJanBuys",
+          state.APIendpoint + "/UploadData?FileName=SupremeLeaderJanBuys",
           requestOptions
       )
           .then((response) => response.text())
           .then((result) => {
             const jsonResult = JSON.parse(result);
-            console.log(jsonResult);
-            console.log(jsonResult.response);
-            dispatch({ type: "SET_DATASET", payload: { dataSet: jsonResult.data } });
-            dispatch({ type: "SET_DATASET_FILENAME", payload: { dataSetFileName: fileObjects[0].name } });
-            dispatch({ type: "SET_DATASET_RESPONSE", payload: { dataSetResponse: jsonResult.response } });
-            dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-
-            //this.setState({sentences: jsonResult.data, sentence: jsonResult.data[0]});
-            history.push("/main");
+            console.log(jsonResult); //Debugging
+            console.log(jsonResult.response); //Debugging
+            dispatch({ type: "SET_DATASET", payload: { dataSet: jsonResult.data } }); //Store the data-set (ids and inputs stored in backend)
+            dispatch({ type: "SET_DATASET_FILENAME", payload: { dataSetFileName: fileObjects[0].name } }); //store name of data-set uploaded
+            dispatch({ type: "SET_DATASET_RESPONSE", payload: { dataSetResponse: jsonResult.response } }); //store response from back-end
+            dispatch({ type: "SET_LOADING", payload: { isLoading: false } }); //Stop the loading animation
+            history.push("/main"); //Take user to the main page
           })
           .catch((error) => {
             console.log("error", error);
             dispatch({ type: "SET_LOADING", payload: { isLoading: false } });
-            history.push("/404"); //for debugging
+            history.push("/404"); //Take user to error page
           });
     }else {
     setOpen(true);
