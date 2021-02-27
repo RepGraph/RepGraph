e5b91310dac6f3ec77b6628dba6e9646734fd138
Roy Cohen <CHNROY002@myuct.ac.za>
2021-02-27 14:23:13 +0200

    Add buttons for decreasing and increasing graph spacing from the zoom portal.
    - Better user experience.

--- src/main/javascript/repgraphfrontend/src/Components/Graph/ZoomPortal.js
+++ src/main/javascript/repgraphfrontend/src/Components/Graph/ZoomPortal.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Zoom } from "@visx/zoom";
-import { RectClipPath } from "@visx/clip-path";
-import { localPoint } from "@visx/event";
+import React, {useState} from "react";
+import {Zoom} from "@visx/zoom";
+import {RectClipPath} from "@visx/clip-path";
+import {localPoint} from "@visx/event";
 
 const initialTransform = {
     scaleX: 1.25,
@@ -13,10 +13,10 @@ const initialTransform = {
 };
 
 const ZoomPortal = (props) => {
-    const { width, height, backgroundColour } = props;
+    const {width, height, backgroundColour} = props;
 
     return (
-        <div style={{ display: "flex", flexGrow: 1, border: "0px solid green" }}>
+        <div style={{display: "flex", flexGrow: 1, border: "0px solid green"}}>
             <Zoom
                 width={width}
                 height={height}
@@ -34,40 +34,80 @@ const ZoomPortal = (props) => {
                 }}
             >
                 {(zoom) => (
-                    <svg
-                        width={width}
-                        height={height}
-                        style={{
-                            cursor: zoom.isDragging ? "grabbing" : "grab",
-                            // border: "8px solid #dedede",
-                            // borderRadius: "8px"
-                        }}
-                        // rx={8}
-                        // ry={8}
-                    >
-                        <rect width={width} height={height} fill={backgroundColour} />
-                        <rect
+                    <div className="relative">
+                        <svg
                             width={width}
                             height={height}
-                            fill={backgroundColour}
-                            onTouchStart={zoom.dragStart}
-                            onTouchMove={zoom.dragMove}
-                            onTouchEnd={zoom.dragEnd}
-                            onMouseDown={zoom.dragStart}
-                            onMouseMove={zoom.dragMove}
-                            onMouseUp={zoom.dragEnd}
-                            onMouseLeave={() => {
-                                if (zoom.isDragging) zoom.dragEnd();
+                            style={{
+                                cursor: zoom.isDragging ? "grabbing" : "grab",
                             }}
-                            onDoubleClick={(event) => {
-                                const point = localPoint(event) || { x: 0, y: 0 };
-                                zoom.scale({ scaleX: 1.1, scaleY: 1.1, point });
-                            }}
-                        />
-                        <g transform={zoom.toString()}>{props.children}</g>
-                    </svg>
+                        >
+                            <rect width={width} height={height} fill={backgroundColour}/>
+                            <rect
+                                width={width}
+                                height={height}
+                                fill={backgroundColour}
+                                onTouchStart={zoom.dragStart}
+                                onTouchMove={zoom.dragMove}
+                                onTouchEnd={zoom.dragEnd}
+                                onMouseDown={zoom.dragStart}
+                                onMouseMove={zoom.dragMove}
+                                onMouseUp={zoom.dragEnd}
+                                onMouseLeave={() => {
+                                    if (zoom.isDragging) zoom.dragEnd();
+                                }}
+                                onDoubleClick={(event) => {
+                                    zoom.center()
+                                }}
+                            />
+                            <g transform={zoom.toString()}>{props.children}</g>
+                        </svg>
+                        <div className="controls">
+                            <button
+                                type="button"
+                                className="btn"
+                                onClick={() => zoom.scale({scaleX: 1.2, scaleY: 1.2})}
+                            >
+                                Increase Spacing
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-bottom"
+                                onClick={() => zoom.scale({scaleX: 0.8, scaleY: 0.8})}
+                            >
+                               Decrease Spacing
+                            </button>
+                        </div>
+                    </div>
                 )}
             </Zoom>
+            <style jsx>{`
+        .btn {
+          margin-top: 10px;
+          text-align: center;
+          border: none;
+          background: #cfcfcf;
+          color: #000000;
+          padding-top: 10px;
+          padding-bottom: 10px;
+          width: 75px;
+          font-size: 15px;
+        }
+        .btn-bottom {
+          margin-bottom: 1rem;
+        }
+        .controls {
+          position: absolute;
+          top: 15px;
+          right: 15px;
+          display: flex;
+          flex-direction: column;
+          align-items: flex-end;
+        } 
+        .relative {
+          position: relative;
+        }
+      `}</style>
         </div>
     );
 };
