9ca1b6e5d71f150005a669952f3056b16c765f81
Jaron Cohen <CHNJAR003@myuct.ac.za>
2021-03-01 20:33:20 +0200

    Change HomePage.js buttons to match rest of frontend

--- src/main/javascript/repgraphfrontend/src/HomePage.js
+++ src/main/javascript/repgraphfrontend/src/HomePage.js
@@ -40,6 +40,7 @@ export default function HomePage(props) {
         console.log(userID); //Debugging
         if (fileObjects.length > 0) {
             let formData = new FormData();
+            console.log("fileObjects", fileObjects);
             formData.append("data", fileObjects[0]);
 
             let myHeaders = new Headers();
@@ -47,7 +48,7 @@ export default function HomePage(props) {
 
             let requestOptions = {
                 method: "POST",
-                headers : myHeaders,
+                headers: myHeaders,
                 body: formData,
                 redirect: "follow"
             };
@@ -56,7 +57,7 @@ export default function HomePage(props) {
 
             //Upload the file chosen by the user
             fetch(
-                state.APIendpoint + "/UploadData?FileName=SupremeLeaderJanBuys&Framework="+state.framework,
+                state.APIendpoint + "/UploadData?FileName=SupremeLeaderJanBuys&Framework=" + state.framework,
                 requestOptions
             )
                 .then((response) => {
@@ -94,13 +95,101 @@ export default function HomePage(props) {
     }
 
     function handleSkip() {
-        history.push("/main");
+
+        // console.log(process.env.PUBLIC_URL + '/dmrsDemoData.dmrs');
+        //
+        // const res = await fetch(process.env.PUBLIC_URL + '/dmrsDemoData.dmrs');
+        // console.log(res);
+
+        // fetch(process.env.PUBLIC_URL + '/dmrsDemoData.dmrs').then(
+        //     response => console.log("response", response)
+        // ).catch(e => console.log(e));
+
+        // //let userID = uuid();
+        // let userID = 1;
+        // dispatch({type: "SET_USER_ID", payload: {userID: userID}}); //Show loading animation while awaiting response
+        //
+        // let demoFile = null;
+        //
+        // switch (state.framework) {
+        //     case 1:
+        //         demoFile = dmrsDemoData;
+        //         break;
+        //     case 2:
+        //         demoFile = "edsDemoData.mrp";
+        //         break;
+        //     case 3:
+        //         demoFile = "ptgDemoData.mrp";
+        //         break;
+        //     case 4:
+        //         demoFile = "uccaDemoData.mrp";
+        //         break;
+        //     case 5:
+        //         demoFile = "amrDemoData.mrp";
+        //         break;
+        //     default:
+        //         demoFile = "dmrsDemoData.dmrs";
+        // }
+        //
+        // console.log(userID); //Debugging
+        //
+        // let formData = new FormData();
+        // console.log("fileObjects", fileObjects);
+        // formData.append("data", fileObjects[0]);
+        //
+        // let myHeaders = new Headers();
+        // myHeaders.append("X-USER", userID);
+        //
+        // let requestOptions = {
+        //     method: "POST",
+        //     headers: myHeaders,
+        //     body: formData,
+        //     redirect: "follow"
+        // };
+        //
+        // dispatch({type: "SET_LOADING", payload: {isLoading: true}}); //Show loading animation while awaiting response
+        //
+        // //Upload the file chosen by the user
+        // fetch(
+        //     state.APIendpoint + "/UploadData?FileName=SupremeLeaderJanBuys&Framework=" + state.framework,
+        //     requestOptions
+        // ).then((response) => {
+        //         console.log(response);
+        //         if (!response.ok) {
+        //             throw "Uploaded File Response not OK";
+        //         }
+        //         return response.text();
+        //     }).then((result) => {
+        //         const jsonResult = JSON.parse(result);
+        //         console.log(jsonResult); //Debugging
+        //         console.log(jsonResult.response); //Debugging
+        //
+        //         dispatch({type: "SET_DATASET", payload: {dataSet: jsonResult.data}}); //Store the data-set (ids and inputs stored in backend)
+        //         dispatch({type: "SET_DATASET_FILENAME", payload: {dataSetFileName: fileObjects[0].name}}); //store name of data-set uploaded
+        //         dispatch({type: "SET_DATASET_RESPONSE", payload: {dataSetResponse: jsonResult.response}}); //store response from back-end
+        //
+        //         dispatch({type: "SET_SENTENCE_GRAPHDATA", payload: {selectedSentenceGraphData: null}});
+        //         dispatch({type: "SET_SENTENCE_VISUALISATION", payload: {selectedSentenceVisualisation: null}});
+        //         dispatch({type: "SET_SELECTED_SENTENCE_ID", payload: {selectedSentenceID: null}});
+        //
+        //         dispatch({type: "SET_LOADING", payload: {isLoading: false}}); //Stop the loading animation
+        //         history.push("/main"); //Take user to the main page
+        //
+        //     })
+        //     .catch((error) => {
+        //         console.log("error", error);
+        //         dispatch({type: "SET_LOADING", payload: {isLoading: false}});
+        //         history.push("/404"); //Take user to error page
+        //     });
+
+        history.push("/main"); //Take user to the main page
+
     }
 
     const handleFormatChange = (event, newFormat) => {
         dispatch({type: "SET_FRAMEWORK", payload: {framework: newFormat}}); //Show loading animation while awaiting response
-
     }
+
     return (
         <Grid
             container
@@ -119,9 +208,6 @@ export default function HomePage(props) {
                 exclusive
                 onChange={handleFormatChange}
                 aria-label="Framework formats"
-
-
-
             >
                 <ToggleButton color="primary" value="1" aria-label="DMRS">
                     <Typography color="textPrimary">DMRS</Typography>
@@ -144,7 +230,7 @@ export default function HomePage(props) {
             </Grid>
             <Grid item style={{minWidth: "50vw"}}>
                 <DropzoneArea
-                    acceptedFiles={[".dmrs",".eds",".amr",".ptg",".ucca",".mrp"]}
+                    acceptedFiles={[".dmrs", ".eds", ".amr", ".ptg", ".ucca", ".mrp"]}
                     dropzoneText={"Drag and drop"}
                     icon={<CloudUploadIcon/>}
                     onChange={(files) => {
@@ -154,11 +240,13 @@ export default function HomePage(props) {
                 />
             </Grid>
             <Grid item>
-                <Button onClick={handleUpload} color={"primary"} variant={"outlined"} startIcon={<CloudUploadIcon/>}>
+                <Button onClick={handleUpload} color="primary" variant="contained" disableElevation
+                        startIcon={<CloudUploadIcon/>}>
                     <Typography variant="h6">Upload</Typography>
                 </Button>
             </Grid>
-            <Grid><Button onClick={handleSkip} color={"primary"} variant={"outlined"} startIcon={<ArrowForwardIcon/>}>
+            <Grid><Button onClick={handleSkip} color="primary" variant="contained" disableElevation
+                          startIcon={<ArrowForwardIcon/>}>
                 <Typography variant="h6">Skip Upload </Typography>
             </Button></Grid>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
